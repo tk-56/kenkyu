cabba5c2769c1aa3da693c4a5ca46f1b00c942d9
tk-56 <ufnfuftv@gmail.com>
2018-09-18 15:07:29 +0900

    create title

---
+++ title/jsonTotxt.py
@@ -0,0 +1,93 @@
+import MeCab
+import json
+import re
+import glob
+
+tagger = MeCab.Tagger("-Owakati")
+filename=glob.glob("../qiita/*.json")
+bodyall=[]
+titleall=[]
+#wordall=[]
+
+print("jsonの読み込みの開始")
+for i in range(len(filename)):
+    print(filename[i])
+    f=open(filename[i],"r")
+    data=json.load(f)
+    f.close()
+    for k in range(len(data)):
+        text=str(data[k]["title"])
+        #title=str(data[k]["title"])
+        text = re.sub(r'https?:\/\/.*?[\r\n ]', '',text)
+        text = re.sub('[.]',' ',text)
+        text = re.sub('[“]',"",text)
+        text = re.sub('[—]',"",text)
+        text = re.sub('[\xa0]','',text)
+        text = re.sub('[[]','',text)
+        text = re.sub('[]]','',text)
+        text = re.sub('[(]','',text)
+        text = re.sub('[)]','',text)
+        text = re.sub('[,]','',text)
+        text = re.sub('[”]','',text)
+        text = re.sub('[−]','',text)
+        text = re.sub('["]','',text)
+        text = re.sub('[////]','',text)
+        text = re.sub('[0-9]','',text)
+        text = re.sub('[:]','',text)
+        text = re.sub('[*]','',text)
+        text = re.sub('[!-/:-@[-`{-~]','',text)
+        text = re.sub(u'[︰-＠]','',text)
+        text = re.sub(r'　', ' ', text)  # 全角空白の除去
+        text = re.sub('[。]','',text)
+
+        text = tagger.parse(text)
+        bodyall.append(text)
+        #titleall.append(title)
+
+number=[]
+for i in range(len(bodyall)):
+    if not bodyall[i]:
+        number.append(i)
+        print("空リストの削除now ")
+for i in number:
+    del bodyall[i]
+    #del titleall[i]
+
+print("削除完了")
+
+print("改行文字の削除中")
+for i in range(len(bodyall)):
+    bodyall[i]=bodyall[i].split()
+    while "n" in bodyall[i]: bodyall[i].remove("n")
+    while "nn" in bodyall[i]: bodyall[i].remove("nn")
+    bodyall[i]=" ".join(bodyall[i])
+print("削除完了")
+
+"""
+tagger = MeCab.Tagger ("-Ochasen")
+for i in range(len(body)):
+    tagger.parse("")
+    node = tagger.parseToNode(body[i])
+    while node:
+        if node.feature[0]=="名":
+            word.append(node.surface)
+        node = node.next
+    word=" ".join(word)
+    wordall.append(word)
+"""
+
+print("ファイルへの書き込み中")
+f = open('text/title.txt', 'w')
+for i in bodyall:
+    f.write(str(i) + "\n")
+    #f.write(str(i))
+f.close()
+
+"""
+f = open('text/title.txt', 'w')
+for i in titleall:
+    f.write(str(i) + "\n")
+    #f.write(str(i))
+f.close()
+"""
+print("システムオールグリーン")
---
+++ title/tfidfcounter.py
@@ -0,0 +1,54 @@
+import re
+from sklearn.feature_extraction.text import TfidfVectorizer
+import numpy as np
+import heapq
+np.set_printoptions(threshold=np.inf)
+
+NUMBER=56 #表示したい記事番号
+
+
+f = open('textM/title.txt', 'r',encoding="utf-8")
+body = f.readlines()
+f.close()
+"""
+f = open('textM/title.txt', 'r',encoding="utf-8")
+title = f.readlines()
+f.close()
+"""
+tfidf_vect = TfidfVectorizer()
+X_tfidf = tfidf_vect.fit_transform(body)
+name=tfidf_vect.get_feature_names()
+gyoretuall=X_tfidf.toarray()
+
+
+"""
+gyoretu=gyoretu[NUMBER]
+print(title[NUMBER])
+for i in range(len(name)):
+    if gyoretu[i] > 0:
+        print(name[i])
+        print(gyoretu[i])
+"""
+dicall={}
+wordlist=[]
+counter=0
+for i in range(len(gyoretuall)):
+    gyoretu=gyoretuall[i]
+    for s in range(len(gyoretu)):
+            if gyoretu[s] > 0:
+                dic={name[s]:gyoretu[s]}
+                dicall.update(dic)
+                counter+=1
+    if counter > 4:
+        sorted_dicall=sorted(dicall.items(), key=lambda x: -x[1])
+        for k in range(5):#その記事の上位単語数
+            wordlist.append(sorted_dicall[k][0])
+    dicall={}
+    counter=0
+
+f = open('countM/tfcount.txt', 'w',encoding="utf-8")
+for i in wordlist:
+    f.write(str(i)+"\n")
+f.close()
+
+print("完了")
